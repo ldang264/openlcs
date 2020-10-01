2d18b81178d1388a277ed8610e6089f90f78b22e
ldang264 <820704815@qq.com>
2020-10-02 07:36:10 +0800

    136

---
+++ src/q00136/SingleNumber_Bit.java
@@ -0,0 +1,49 @@
+package q00136;
+
+/**
+ * 给定一个非空整数数组，除了某个元素只出现一次以外，其余每个元素均出现两次。找出那个只出现了一次的元素。
+ *
+ * 说明：
+ *
+ * 你的算法应该具有线性时间复杂度。 你可以不使用额外空间来实现吗？
+ *
+ * 示例 1:
+ *
+ * 输入: [2,2,1]
+ * 输出: 1
+ * 示例 2:
+ *
+ * 输入: [4,1,2,1,2]
+ * 输出: 4
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/single-number
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
+/**
+ *
+ 作者：LeetCode-Solution
+ 链接：https://leetcode-cn.com/problems/single-number/solution/zhi-chu-xian-yi-ci-de-shu-zi-by-leetcode-solution/
+ 来源：力扣（LeetCode）
+ 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+ */
+public class SingleNumber_Bit {
+
+    public static void main(String[] args) {
+        SingleNumber_Bit sn = new SingleNumber_Bit();
+        int[] nums1 = new int[]{2, 2, 1};
+        int[] nums2 = new int[]{4, 1, 2, 1, 2};
+        System.out.println(sn.singleNumber(nums1));
+        System.out.println(sn.singleNumber(nums2));
+    }
+
+    public int singleNumber(int[] nums) {
+        int single = 0;
+        for (int num : nums) {
+            single ^= num;
+        }
+        return single;
+
+    }
+}
