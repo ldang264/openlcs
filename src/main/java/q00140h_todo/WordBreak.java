f38a87fb3c890d76be0c65bd3267efb8e7290efd
ldang264 <820704815@qq.com>
2020-11-01 20:44:25 +0800

    140

---
+++ src/main/java/q00140h_todo/WordBreak.java
@@ -0,0 +1,50 @@
+package q00140h_todo;
+
+import java.util.List;
+
+/**
+ * 给定一个非空字符串 s 和一个包含非空单词列表的字典 wordDict，在字符串中增加空格来构建一个句子，使得句子中所有的单词都在词典中。返回所有这些可能的句子。
+ *
+ * 说明：
+ *
+ * 分隔时可以重复使用字典中的单词。
+ * 你可以假设字典中没有重复的单词。
+ * 示例 1：
+ *
+ * 输入:
+ * s = "catsanddog"
+ * wordDict = ["cat", "cats", "and", "sand", "dog"]
+ * 输出:
+ * [
+ *   "cats and dog",
+ *   "cat sand dog"
+ * ]
+ * 示例 2：
+ *
+ * 输入:
+ * s = "pineapplepenapple"
+ * wordDict = ["apple", "pen", "applepen", "pine", "pineapple"]
+ * 输出:
+ * [
+ *   "pine apple pen apple",
+ *   "pineapple pen apple",
+ *   "pine applepen apple"
+ * ]
+ * 解释: 注意你可以重复使用字典中的单词。
+ * 示例 3：
+ *
+ * 输入:
+ * s = "catsandog"
+ * wordDict = ["cats", "dog", "sand", "and", "cat"]
+ * 输出:
+ * []
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/word-break-ii
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class WordBreak {
+    public List<String> wordBreak(String s, List<String> wordDict) {
+        return null;
+    }
+}
