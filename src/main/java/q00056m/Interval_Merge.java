7c85afdd6069deca905982bea2f9075ae446b61a
ldang264 <820704815@qq.com>
2020-11-04 11:05:54 +0800

    57

--- src/main/java/q00056m/Interval_Merge.java
+++ src/main/java/q00056m/Interval_Merge.java
@@ -1,5 +1,8 @@
 package q00056m;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 /**
  * 给出一个区间的集合，请合并所有重叠的区间。
  *
@@ -26,11 +29,11 @@ package q00056m;
  * 链接：https://leetcode-cn.com/problems/merge-intervals
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
-public class Merge {
+public class Interval_Merge {
     public int[][] merge(int[][] intervals) {
         if (intervals == null || intervals.length == 0) return intervals;
         int[][] ans = new int[intervals.length][];
-        sort(intervals,0, intervals.length - 1);
+        Arrays.sort(intervals, Comparator.comparingInt(o -> o[0]));
         int index = 0;
         for (int i = 0; i < intervals.length - 1; i++) {
             int[] prev = intervals[i];
@@ -50,54 +53,5 @@ public class Merge {
             realAns[i] = ans[i];
         }
         return realAns;
-    }
-
-    /**
-     * 快速排序
-     * @param array
-     * @param start
-     * @param end
-     * @return
-     */
-    private static int[][] sort(int[][] array, int start, int end) {
-        if (start < 0 || end >= array.length || start > end) return null;
-        int smallIndex = partition(array, start, end);
-        if (smallIndex > start)
-            sort(array, start, smallIndex - 1);
-        if (smallIndex < end)
-            sort(array, smallIndex + 1, end);
-        return array;
-    }
-
-    /**
-     * 快速排序算法——partition
-     * @param array
-     * @param start
-     * @param end
-     * @return
-     */
-    private static int partition(int[][] array, int start, int end) {
-        int pivot = (int) (start + Math.random() * (end - start + 1));
-        int smallIndex = start - 1;
-        swap(array, pivot, end);
-        for (int i = start; i <= end; i++)
-            if (array[i][0] <= array[end][0]) {
-                smallIndex++;
-                if (i > smallIndex)
-                    swap(array, i, smallIndex);
-            }
-        return smallIndex;
-    }
-
-    /**
-     * 交换数组内两个元素
-     * @param array
-     * @param i
-     * @param j
-     */
-    private static void swap(int[][] array, int i, int j) {
-        int[] temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
     }
 }
---
+++ src/test/java/q00057h/Interval_InsertTest.java
@@ -0,0 +1,76 @@
+package q00057h;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+
+public class Interval_InsertTest {
+
+    Interval_Insert i = new Interval_Insert();
+
+    @Test
+    public void insert() {
+        int[][] intervals = new int[][]{{1,3},{6,9}};
+        int[] newInterval = new int[]{2,5};
+        String exp = "[[1, 5], [6, 9]]";
+        String act = Arrays.deepToString(i.insert(intervals, newInterval));
+        Assert.assertEquals(exp, act);
+    }
+
+    @Test
+    public void insert1() {
+        int[][] intervals = new int[][]{{1,2},{3,5},{6,7},{8,10},{12,16}};
+        int[] newInterval = new int[]{4,8};
+        String exp = "[[1, 2], [3, 10], [12, 16]]";
+        String act = Arrays.deepToString(i.insert(intervals, newInterval));
+        Assert.assertEquals(exp, act);
+    }
+
+    @Test
+    public void insert2() {
+        int[][] intervals = new int[][]{{3,5},{6,7}};
+        int[] newInterval = new int[]{1,2};
+        String exp = "[[1, 2], [3, 5], [6, 7]]";
+        String act = Arrays.deepToString(i.insert(intervals, newInterval));
+        Assert.assertEquals(exp, act);
+    }
+
+    @Test
+    public void insert3() {
+        int[][] intervals = new int[][]{{3,5},{6,7}};
+        int[] newInterval = new int[]{1,3};
+        String exp = "[[1, 5], [6, 7]]";
+        String act = Arrays.deepToString(i.insert(intervals, newInterval));
+        Assert.assertEquals(exp, act);
+    }
+
+    @Test
+    public void insert4() {
+        int[][] intervals = new int[][]{{3,5},{6,7}};
+        int[] newInterval = new int[]{1,9};
+        String exp = "[[1, 9]]";
+        String act = Arrays.deepToString(i.insert(intervals, newInterval));
+        Assert.assertEquals(exp, act);
+    }
+
+    @Test
+    public void insert5() {
+        int[][] intervals = new int[][]{{1,5}};
+        int[] newInterval = new int[]{4,8};
+        String exp = "[[1, 8]]";
+        String act = Arrays.deepToString(i.insert(intervals, newInterval));
+        Assert.assertEquals(exp, act);
+    }
+
+    @Test
+    public void insert6() {
+        int[][] intervals = new int[][]{{1,5}};
+        int[] newInterval = new int[]{6,8};
+        String exp = "[[1, 5], [6, 8]]";
+        String act = Arrays.deepToString(i.insert(intervals, newInterval));
+        Assert.assertEquals(exp, act);
+    }
+}
