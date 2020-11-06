0157f497055895fddae3b69c4117307c8ef5f68c
ldang264 <820704815@qq.com>
2020-11-06 16:43:37 +0800

    16

---
+++ src/test/java/q00016m_todo/ThreeSumClosestTest.java
@@ -0,0 +1,15 @@
+package q00016m_todo;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ThreeSumClosestTest {
+
+    ThreeSumClosest tsc = new ThreeSumClosest();
+
+    @Test
+    public void testThreeSumClosest() {
+        Assert.assertEquals(2, tsc.threeSumClosest(new int[]{-1,2,1,-4}, 1));
+    }
+
+}
