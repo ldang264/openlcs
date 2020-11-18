e8fe9a08abd46b6c02c188e0bb14123089ce215b
ldang264 <820704815@qq.com>
2020-11-18 11:36:56 +0800

    j3

--- src/main/java/q00136s/SingleNumber.java
+++ src/main/java/q00136s/SingleNumber.java
@@ -1,8 +1,5 @@
 package q00136s;
 
-import java.util.HashSet;
-import java.util.Set;
-
 /**
  * 给定一个非空整数数组，除了某个元素只出现一次以外，其余每个元素均出现两次。找出那个只出现了一次的元素。
  *
@@ -34,17 +31,10 @@ public class SingleNumber {
     }
 
     public int singleNumber(int[] nums) {
-        Set<Integer> set = new HashSet<>();
-        for (int num : nums) {
-            if (set.contains(num)) {
-                set.remove(num);
-            } else {
-                set.add(num);
-            }
+        int ans = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            ans ^= nums[i];
         }
-        for (Integer i : set) {
-            return i;
-        }
-        return 0;
+        return ans;
     }
 }
