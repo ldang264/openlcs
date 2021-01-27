8dbd9caff6c0b3c9d2fd449a955bdc46c5c3ef30
ldang264 <820704815@qq.com>
2021-01-27 19:08:13 +0800

    64

---
+++ src/main/java/Q00064m.java
@@ -0,0 +1,41 @@
+/**
+ * 给定一个包含非负整数的 mxn网格grid ，请找出一条从左上角到右下角的路径，使得路径上的数字总和为最小。
+ * 说明：每次只能向下或者向右移动一步。
+ *
+ * 示例 1：
+ * 输入：grid = [[1,3,1],[1,5,1],[4,2,1]]
+ * 输出：7
+ * 解释：因为路径 1→3→1→1→1 的总和最小。
+ *
+ * 示例 2：
+ * 输入：grid = [[1,2,3],[4,5,6]]
+ * 输出：12
+ *
+ * 提示：
+ * m == grid.length
+ * n == grid[i].length
+ * 1 <= m, n <= 200
+ * 0 <= grid[i][j] <= 100
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/minimum-path-sum
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Q00064m {
+    public int minPathSum(int[][] grid) {
+        int[][] dp = new int[grid.length][grid[0].length];
+        dp[0][0] = grid[0][0];
+        for (int i = 1; i < grid.length ; i++) {
+            dp[i][0] = grid[i][0] + dp[i - 1][0];
+        }
+        for (int j = 1; j < grid[0].length ; j++) {
+            dp[0][j] = grid[0][j] + dp[0][j - 1];
+        }
+        for (int i = 1; i < grid.length ; i++) {
+            for (int j = 1; j < grid[0].length; j++) {
+                dp[i][j] = grid[i][j] + Math.min(dp[i - 1][j], dp[i][j - 1]); // 取上边和左边的最小值
+            }
+        }
+        return dp[grid.length - 1][grid[0].length - 1];
+    }
+}
