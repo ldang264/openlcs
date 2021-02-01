959334ee7496338111098f2b1a19cb8256ab44d7
ldang264 <820704815@qq.com>
2021-02-01 10:32:03 +0800

    55

---
+++ src/main/java/Q00055m.java
@@ -0,0 +1,44 @@
+/**
+ * 给定一个非负整数数组nums ，你最初位于数组的 第一个下标 。
+ * 数组中的每个元素代表你在该位置可以跳跃的最大长度。
+ * 判断你是否能够到达最后一个下标。
+ *
+ * 示例1：
+ * 输入：nums = [2,3,1,1,4]
+ * 输出：true
+ * 解释：可以先跳 1 步，从下标 0 到达下标 1, 然后再从下标 1 跳 3 步到达最后一个下标。
+ *
+ * 示例2：
+ * 输入：nums = [3,2,1,0,4]
+ * 输出：false
+ * 解释：无论怎样，总会到达下标为 3 的位置。但该下标的最大跳跃长度是 0 ， 所以永远不可能到达最后一个下标。
+ *
+ * 提示：
+ *
+ * 1 <= nums.length <= 3 * 104
+ * 0 <= nums[i] <= 105
+ *
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/jump-game
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
+/**
+ * 使用回溯法时出现超时，因为做了太多的无效递归
+ * 使用贪婪算法，在遍历过程中努力向后寻找最远的下标
+ */
+public class Q00055m {
+    public boolean canJump(int[] nums) {
+        if (nums.length == 1) return true;
+        int maxIndex = nums[0]; // 初始可达最远下标
+        for (int i = 1; i <= maxIndex; i++) {
+            int j = nums[i] + i;
+            if (j >= nums.length - 1) return true;
+            if (maxIndex < j) {
+                maxIndex = j; // 取可达最远下标的较大值
+            }
+        }
+        return false;
+    }
+}
---
+++ src/test/java/Q00055mTest.java
@@ -0,0 +1,19 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Q00055mTest {
+
+    Q00055m q00055m = new Q00055m();
+
+    @Test
+    public void canJump() {
+        Assert.assertTrue(q00055m.canJump(new int[]{0}));
+        Assert.assertFalse(q00055m.canJump(new int[]{0, 1}));
+        Assert.assertTrue(q00055m.canJump(new int[]{3, 0}));
+        Assert.assertTrue(q00055m.canJump(new int[]{3, 2, 3, 0, 0, 1}));
+        Assert.assertTrue(q00055m.canJump(new int[]{2,3,1,1,4}));
+        Assert.assertFalse(q00055m.canJump(new int[]{3,2,1,0,4}));
+    }
+}
