9eb15f3955094323da81391f83269441b6832fd1
ldang264 <820704815@qq.com>
2021-03-12 16:58:38 +0800

    57

--- src/main/java/Q00057m.java
+++ src/main/java/Q00057m.java
@@ -1,29 +1,29 @@
 /**
  * 给你一个 无重叠的 ，按照区间起始端点排序的区间列表。
- *
+ * <p>
  * 在列表中插入一个新的区间，你需要确保列表中的区间仍然有序且不重叠（如果有必要的话，可以合并区间）。
- *
+ * <p>
  * 示例1：
  * 输入：intervals = [[1,3],[6,9]], newInterval = [2,5]
  * 输出：[[1,5],[6,9]]
- *
+ * <p>
  * 示例 2：
  * 输入：intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]], newInterval = [4,8]
  * 输出：[[1,2],[3,10],[12,16]]
  * 解释：这是因为新的区间 [4,8] 与 [3,5],[6,7],[8,10]重叠。
- *
+ * <p>
  * 示例 3：
  * 输入：intervals = [], newInterval = [5,7]
  * 输出：[[5,7]]
- *
+ * <p>
  * 示例 4：
  * 输入：intervals = [[1,5]], newInterval = [2,3]
  * 输出：[[1,5]]
- *
+ * <p>
  * 示例 5：
  * 输入：intervals = [[1,5]], newInterval = [2,7]
  * 输出：[[1,7]]
- *
+ * <p>
  * 提示：
  * 0 <= intervals.length <= 104
  * intervals[i].length == 2
@@ -31,13 +31,30 @@
  * intervals 根据 intervals[i][0] 按 升序 排列
  * newInterval.length == 2
  * 0 <=newInterval[0] <=newInterval[1] <= 105
- *
+ * <p>
  * 来源：力扣（LeetCode）
  * 链接：https://leetcode-cn.com/problems/insert-interval
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 public class Q00057m {
     public int[][] insert(int[][] intervals, int[] newInterval) {
+        if (intervals.length == 0) return new int[][]{newInterval};
+        int left = -1;
+        int right = -1;
+        for (int i = 0; i < intervals.length; i++) {
+            if (newInterval[0] < intervals[i][0]) break;
+            if (newInterval[0] <= intervals[i][1]) {
+                left = i;
+                break;
+            }
+        }
+        for (int i = left == -1 ? 0 : left; i < intervals.length; i++) {
+            if (newInterval[1] < intervals[i][0]) break;
+            if (newInterval[1] <= intervals[i][1]) {
+                right = i;
+                break;
+            }
+        }
         return null;
     }
 }
