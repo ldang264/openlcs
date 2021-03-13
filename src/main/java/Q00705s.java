001b05f3d136bfb0bb72e0ec4fda8eff36beda16
ldang264 <820704815@qq.com>
2021-03-14 07:41:44 +0800

    705

---
+++ src/main/java/Q00705s.java
@@ -0,0 +1,63 @@
+/**
+ * 不使用任何内建的哈希表库设计一个哈希集合（HashSet）。
+ *
+ * 实现 MyHashSet 类：
+ *
+ * void add(key) 向哈希集合中插入值 key 。
+ * bool contains(key) 返回哈希集合中是否存在这个值 key 。
+ * void remove(key) 将给定值 key 从哈希集合中删除。如果哈希集合中没有这个值，什么也不做。
+ * 
+ * 示例：
+ *
+ * 输入：
+ * ["MyHashSet", "add", "add", "contains", "contains", "add", "contains", "remove", "contains"]
+ * [[], [1], [2], [1], [3], [2], [2], [2], [2]]
+ * 输出：
+ * [null, null, null, true, false, null, true, null, false]
+ *
+ * 解释：
+ * MyHashSet myHashSet = new MyHashSet();
+ * myHashSet.add(1);      // set = [1]
+ * myHashSet.add(2);      // set = [1, 2]
+ * myHashSet.contains(1); // 返回 True
+ * myHashSet.contains(3); // 返回 False ，（未找到）
+ * myHashSet.add(2);      // set = [1, 2]
+ * myHashSet.contains(2); // 返回 True
+ * myHashSet.remove(2);   // set = [1]
+ * myHashSet.contains(2); // 返回 False ，（已移除）
+ * 
+ *
+ * 提示：
+ *
+ * 0 <= key <= 106
+ * 最多调用 104 次 add、remove 和 contains 。
+ * 
+ *
+ * 进阶：你可以不使用内建的哈希集合库解决此问题吗？
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/design-hashset
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Q00705s {
+
+    private boolean[] set;
+
+    /** Initialize your data structure here. */
+    public Q00705s() {
+        set = new boolean[100_0001];
+    }
+
+    public void add(int key) {
+        set[key] = true;
+    }
+
+    public void remove(int key) {
+        set[key] = false;
+    }
+
+    /** Returns true if this set contains the specified element */
+    public boolean contains(int key) {
+        return set[key];
+    }
+}
