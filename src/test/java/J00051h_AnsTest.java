575ed0fe59f2f943aa6d3dacfa52b81804f79135
ldang264 <820704815@qq.com>
2021-03-29 18:50:38 +0800

    51

---
+++ src/test/java/J00051h_AnsTest.java
@@ -0,0 +1,19 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class J00051h_AnsTest {
+
+    J00051h_Ans j00051h_ans = new J00051h_Ans();
+
+    @Test
+    public void reversePairs() {
+        Assert.assertEquals(j00051h_ans.reversePairs(new int[]{7,5,6,4}), 5);
+    }
+
+    @Test
+    public void reversePairs2() {
+        Assert.assertEquals(j00051h_ans.reversePairs(new int[]{13,5,9,2}), 5);
+    }
+}
