6f44079937b81fbb9ac9b5bbddb761328f0020e0
ldang264 <820704815@qq.com>
2021-04-06 19:11:46 +0800

    leetcodes

--- src/main/java/algorithm/tree/DFS.java
+++ src/main/java/algorithm/tree/DFS.java
@@ -2,68 +2,110 @@ package algorithm.tree;
 
 import tool.TreeNode;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /**
  * 深度优先搜索
  */
 public class DFS {
 
-    public List<Integer> preorderTraversal(TreeNode root) {
-        if (root == null) return new ArrayList<>(0);
-        List<Integer> list = new ArrayList<>();
-        dfs_preorder(root, list);
-        //dfs_inorder(root, list);
-        //dfs_postorder(root, list);
-        return list;
+    /**
+     * 迭代-前序遍历
+     * @param root
+     */
+    public void dfs_iteration_preorder(TreeNode root) {
+        Deque<TreeNode> stack = new LinkedList<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            root = stack.pop();
+            System.out.print(root.val + " ");
+            if (root.right != null) {
+                stack.push(root.right);
+            }
+            if (root.left != null) {
+                stack.push(root.left);
+            }
+        }
+    }
+
+    /**
+     * 迭代-中序遍历
+     * @param root
+     */
+    public void dfs_iteration_inorder(TreeNode root) {
+        Deque<TreeNode> stack = new LinkedList<>();
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            System.out.print(root.val + " ");
+            root = root.right;
+        }
+    }
+
+    /**
+     * 迭代-后序遍历
+     * @param root
+     */
+    public void dfs_iteration_postorder(TreeNode root) {
+        Deque<TreeNode> stack = new LinkedList<>();
+        TreeNode curr = root;
+        TreeNode last = null;
+        while (curr != null || !stack.isEmpty()) {
+            while (curr != null) {
+                stack.push(curr);
+                curr = curr.left;
+            }
+            curr = stack.peek();
+            if (curr.right == null || curr.right == last) {
+                System.out.print(curr.val + " ");
+                stack.pop();
+                last = curr;
+                curr = null;
+            } else {
+                curr = curr.right;
+            }
+        }
     }
 
     /**
      * 递归-前序遍历
-     * @param node
-     * @param list
+     * @param root
      * @return
      */
-    private void dfs_preorder(TreeNode node, List<Integer> list) {
-        list.add(node.val); // 根
-        if (node.left != null) {
-            dfs_preorder(node.left, list); // 左
-        }
-        if (node.right != null) {
-            dfs_preorder(node.right, list); // 右
+    public void dfs_recursion_preorder(TreeNode root) {
+        if (root != null) {
+            System.out.print(root.val + " "); // 根
+            dfs_recursion_preorder(root.left); // 左
+            dfs_recursion_preorder(root.right); // 右
         }
     }
 
     /**
      * 递归-中序遍历
-     * @param node
-     * @param list
+     * @param root
      * @return
      */
-    private void dfs_inorder(TreeNode node, List<Integer> list) {
-        if (node.left != null) {
-            dfs_preorder(node.left, list); // 左
-        }
-        list.add(node.val); // 根
-        if (node.right != null) {
-            dfs_preorder(node.right, list); // 右
+    public void dfs_recursion_inorder(TreeNode root) {
+        if (root != null) {
+            dfs_recursion_inorder(root.left); // 左
+            System.out.print(root.val + " "); // 根
+            dfs_recursion_inorder(root.right); // 右
         }
     }
 
     /**
      * 递归-后序遍历
-     * @param node
-     * @param list
+     * @param root
      * @return
      */
-    private void dfs_postorder(TreeNode node, List<Integer> list) {
-        if (node.left != null) {
-            dfs_preorder(node.left, list); // 左
+    public void dfs_recursion_postorder(TreeNode root) {
+        if (root != null) {
+            dfs_recursion_postorder(root.left); // 左
+            dfs_recursion_postorder(root.right); // 右
+            System.out.print(root.val + " "); // 根
         }
-        if (node.right != null) {
-            dfs_preorder(node.right, list); // 右
-        }
-        list.add(node.val); // 根
     }
 }
---
+++ src/test/java/algorithm/tree/DFSTest.java
@@ -0,0 +1,55 @@
+package algorithm.tree;
+
+import org.junit.Test;
+import tool.TreeNode;
+
+public class DFSTest {
+
+    DFS dfs = new DFS();
+
+    /**
+     *      1
+     *     / \
+     *    2   3
+     *    \    \
+     *     4    5
+     *
+     */
+    TreeNode root = TreeNode.deserialize("1,2,3,null,4,null,5");
+
+    @Test
+    public void dfs_iteration_preorder() {
+        dfs.dfs_iteration_preorder(root); // 1 2 4 3 5
+        System.out.println();
+    }
+
+    @Test
+    public void dfs_iteration_inorder() {
+        dfs.dfs_iteration_inorder(root); // 2 4 1 3 5
+        System.out.println();
+    }
+
+    @Test
+    public void dfs_iteration_postorder() {
+        dfs.dfs_iteration_postorder(root); // 4 2 5 3 1
+        System.out.println();
+    }
+
+    @Test
+    public void dfs_recursion_preorder() {
+        dfs.dfs_recursion_preorder(root); // 1 2 4 3 5
+        System.out.println();
+    }
+
+    @Test
+    public void dfs_recursion_inorder() {
+        dfs.dfs_recursion_inorder(root); // 2 4 1 3 5
+        System.out.println();
+    }
+
+    @Test
+    public void dfs_recursion_postorder() {
+        dfs.dfs_recursion_postorder(root); // 4 2 5 3 1
+        System.out.println();
+    }
+}
