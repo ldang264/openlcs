4e1212dd794db495d61eb8beee789a842f3a8315
ldang264 <820704815@qq.com>
2021-04-07 09:32:42 +0800

    leetcodes

--- src/main/java/Q00148m_12.java
+++ src/main/java/Q00148m_12.java
@@ -4,33 +4,25 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 /**
- * 给你链表的头结点 head ，请将其按 升序 排列并返回 排序后的链表 。
- *
+ * 给你链表的头结点head，请将其按 升序 排列并返回 排序后的链表 。
  * 进阶：
- *
- * 你可以在 O(n log n) 时间复杂度和常数级空间复杂度下，对链表进行排序吗？
- *  
+ * 你可以在O(nlogn) 时间复杂度和常数级空间复杂度下，对链表进行排序吗？
  *
  * 示例 1：
- *
- *
  * 输入：head = [4,2,1,3]
  * 输出：[1,2,3,4]
+ *
  * 示例 2：
- *
- *
  * 输入：head = [-1,5,3,4,0]
  * 输出：[-1,0,3,4,5]
- * 示例 3：
  *
+ * 示例 3：
  * 输入：head = []
  * 输出：[]
- *  
  *
  * 提示：
- *
- * 链表中节点的数目在范围 [0, 5 * 104] 内
- * -105 <= Node.val <= 105
+ * 链表中节点的数目在范围[0, 5 * 104]内
+ * -105<= Node.val <= 105
  *
  * 来源：力扣（LeetCode）
  * 链接：https://leetcode-cn.com/problems/sort-list
@@ -107,24 +99,4 @@ public class Q00148m_12 {
         }
         return result;
     }
-
-    /*
-    public ListNode sortList1(ListNode head) {
-        if (head == null) return head;
-        ArrayList<Integer> list = new ArrayList<>();
-        while(head != null) {
-            list.add(head.val);
-            head = head.next;
-        }
-        Integer[] array = list.toArray(new Integer[list.size()]);
-        HeapSort.sort(array);
-        ListNode ans = new ListNode(0);
-        ListNode curr = ans;
-        for (int i = 0; i < array.length; i++) {
-            curr.next = new ListNode(array[i]);
-            curr = curr.next;
-        }
-        return ans.next;
-    }
-    */
 }
