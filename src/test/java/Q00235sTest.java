88f64b46cd6af25f83c798dce7b9f0e5491770e8
ldang264 <820704815@qq.com>
2021-04-11 12:04:12 +0800

    leetcodes

---
+++ src/test/java/Q00235sTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Q00235sTest {
+
+    Q00235s q00235s = new Q00235s();
+
+    @Test
+    public void lowestCommonAncestor() {
+
+    }
+}
