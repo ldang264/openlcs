a324f1b626101bc549f5449e14890ccc1bb9468d
ldang264 <820704815@qq.com>
2021-04-16 16:41:22 +0800

    leetcodes

---
+++ src/main/java/M00016_01s.java
@@ -0,0 +1,28 @@
+/**
+ * 编写一个函数，不用临时变量，直接交换numbers = [a, b]中a与b的值。
+ *
+ * 示例：
+ * 输入: numbers = [1,2]
+ * 输出: [2,1]
+ *
+ * 提示：
+ * numbers.length == 2
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/swap-numbers-lcci
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class M00016_01s {
+
+    /**
+     * 使用异或的结合律
+     * @param numbers
+     * @return
+     */
+    public int[] swapNumbers(int[] numbers) {
+        numbers[0] ^= numbers[1];
+        numbers[1] ^= numbers[0];
+        numbers[0] ^= numbers[1];
+        return numbers;
+    }
+}
