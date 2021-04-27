71cde4dac85c2f89a39e5ad99137cd4a5c318bd1
ldang264 <820704815@qq.com>
2021-04-27 14:37:25 +0800

    leetcodes

--- src/main/java/Q00064m.java
+++ src/main/java/Q00064m.java
@@ -23,20 +23,19 @@
  */
 public class Q00064m {
     public int minPathSum(int[][] grid) {
-        int[][] dp = new int[grid.length][grid[0].length];
-        dp[0][0] = grid[0][0];
-        for (int i = 1; i < grid.length ; i++) {
-            dp[i][0] = grid[i][0] + dp[i - 1][0];
+        int m = grid.length - 1, n = grid[0].length - 1;
+        for (int i = 1; i <= m; i++) {
+            grid[i][0] += grid[i - 1][0];
         }
-        for (int j = 1; j < grid[0].length ; j++) {
-            dp[0][j] = grid[0][j] + dp[0][j - 1];
+        for (int j = 1; j <= n; j++) {
+            grid[0][j] += grid[0][j - 1];
         }
-        for (int i = 1; i < grid.length ; i++) {
-            for (int j = 1; j < grid[0].length; j++) {
-                dp[i][j] = grid[i][j] + Math.min(dp[i - 1][j], dp[i][j - 1]); // 取上边和左边的最小值
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                grid[i][j] += Math.min(grid[i - 1][j], grid[i][j - 1]); // 取上边和左边的最小值
                 
             }
         }
-        return dp[grid.length - 1][grid[0].length - 1];
+        return grid[m][n];
     }
 }
--- src/test/java/Q00064mTest.java
+++ src/test/java/Q00064mTest.java
@@ -1,15 +1,13 @@
 import org.junit.Assert;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 public class Q00064mTest {
 
     Q00064m q00064m = new Q00064m();
 
     @Test
     public void minPathSum() {
-        Assert.assertEquals(12, q00064m.minPathSum(new int[][]{{1,2,3},{4,5,6}}));
-        Assert.assertEquals(7, q00064m.minPathSum(new int[][]{{1,3,1},{1,5,1}, {4,2,1}}));
+        Assert.assertEquals(12, q00064m.minPathSum(new int[][]{{1, 2, 3}, {4, 5, 6}}));
+        Assert.assertEquals(7, q00064m.minPathSum(new int[][]{{1, 3, 1}, {1, 5, 1}, {4, 2, 1}}));
     }
 }
