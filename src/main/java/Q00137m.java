872479c3390b4bca32a3c5efad860f6da941c3d6
ldang264 <820704815@qq.com>
2021-05-11 17:15:20 +0800

    leetcodes

---
+++ src/main/java/Q00137m.java
@@ -0,0 +1,34 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 给你一个整数数组 nums ，除某个元素仅出现 一次 外，其余每个元素都恰出现 三次 。请你找出并返回那个只出现了一次的元素。
+ *
+ * 示例 1：
+ * 输入：nums = [2,2,3,2]
+ * 输出：3
+ *
+ * 示例 2：
+ * 输入：nums = [0,1,0,1,0,1,99]
+ * 输出：99
+ *
+ * 提示：
+ * 1 <= nums.length <= 3 * 104
+ * -231 <= nums[i] <= 231 - 1
+ * nums 中，除某个元素仅出现 一次 外，其余每个元素都恰出现 三次
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/single-number-ii
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Q00137m {
+    public int singleNumber(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>(nums.length / 3 + 1);
+        for (int num : nums) {
+            if (map.merge(num, 1, Integer::sum) == 3) {
+                map.remove(num); // 3个的时候就移除，最后只会剩余1个
+            }
+        }
+        return map.keySet().iterator().next();
+    }
+}
