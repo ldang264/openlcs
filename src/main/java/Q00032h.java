f133fb53a7fa3a07294bb970282744d964ca4fb3
ldang264 <820704815@qq.com>
2021-05-17 09:22:30 +0800

    leetcodes

--- src/main/java/Q00032h.java
+++ src/main/java/Q00032h.java
@@ -1,32 +1,55 @@
-
-
 /**
  * 给定一个只包含 '('和 ')'的字符串，找出最长的包含有效括号的子串的长度。
- * <p>
+ *  
  * 示例1:
- * <p>
  * 输入: "(()"
  * 输出: 2
  * 解释: 最长有效括号子串为 "()"
+ *
  * 示例 2:
- * <p>
  * 输入: ")()())"
  * 输出: 4
  * 解释: 最长有效括号子串为 "()()"
- * <p>
+ *
+ * 示例 3：
+ * 输入：s = ""
+ * 输出：0
+ *
+ * 提示：
+ * 0 <= s.length <= 3 * 104
+ * s[i] 为 '(' 或 ')'
+ *
  * 来源：力扣（LeetCode）
  * 链接：https://leetcode-cn.com/problems/longest-valid-parentheses
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 public class Q00032h {
 
+    public int longestValidParenthesesDp(String s) {
+        if (s.length() < 2) return 0;
+        int[] dp = new int[s.length()];
+        int ans = 0, j;
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) == ')') {
+                if (s.charAt(i - 1) == '(') { // 如果前一个字符就是左括号，则与dp[i-1]连成更长的有效括号
+                    dp[i] = (i >= 2 ? dp[i - 2] : 0) + 2;
+                } else if ((j = i - dp[i - 1] - 1) >= 0 && s.charAt(j) == '(') { // 如果i-dp[i-1]-1那个字符是左括号，则与它连成更长的有效括号
+                    dp[i] = (j >= 1 ? dp[j - 1] : 0) + dp[i - 1] + 2;
+                }
+                ans = Math.max(ans, dp[i]);
+            }
+        }
+        return ans;
+    }
+
     public int longestValidParentheses(String s) {
         if (s == null || s.length() < 2) return 0;
         char[] sa = new char[s.length()];
         int ans = 0, sum = 0, index = 0, start = 0, end = 0;
         for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == '(') {
-                sa[index] = s.charAt(i);
+            char c = s.charAt(i);
+            if (c == '(') {
+                sa[index] = c;
                 start = index++; // 更新(出现的位置
             } else {
                 boolean find = false;
--- src/test/java/Q00032hTest.java
+++ src/test/java/Q00032hTest.java
@@ -1,8 +1,6 @@
 import org.junit.Assert;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 public class Q00032hTest {
     
     Q00032h q00032h = new Q00032h();
@@ -15,5 +13,15 @@ public class Q00032hTest {
         Assert.assertEquals(q00032h.longestValidParentheses("()(()"), 2); // 2
         Assert.assertEquals(q00032h.longestValidParentheses(")()())"), 4); // 4
         Assert.assertEquals(q00032h.longestValidParentheses(")()())()()("), 4); // 4
+    }
+
+    @Test
+    public void longestValidParenthesesDp() {
+        Assert.assertEquals(q00032h.longestValidParenthesesDp("(()"), 2); // 2
+        Assert.assertEquals(q00032h.longestValidParenthesesDp(")()())"), 4); // 4
+        Assert.assertEquals(q00032h.longestValidParenthesesDp("()(())"), 6); // 6
+        Assert.assertEquals(q00032h.longestValidParenthesesDp("()(()"), 2); // 2
+        Assert.assertEquals(q00032h.longestValidParenthesesDp(")()())"), 4); // 4
+        Assert.assertEquals(q00032h.longestValidParenthesesDp(")()())()()("), 4); // 4
     }
 }
