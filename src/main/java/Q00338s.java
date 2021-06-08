4fb1602ee81327635bb09f0106afda92a2e8245f
ldang264 <820704815@qq.com>
2021-06-08 21:56:18 +0800

    leetcodes

--- src/main/java/Q00338s.java
+++ src/main/java/Q00338s.java
@@ -18,7 +18,7 @@
  * 链接：https://leetcode-cn.com/problems/counting-bits
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
-public class Q00338m {
+public class Q00338s {
     public int[] countBits(int num) {
         int[] ans = new int[num + 1];
         ans[0] = 0;
