d0d9054b150e432921ab2768f43030ea7141ba87
ldang264 <820704815@qq.com>
2021-06-23 18:36:36 +0800

    leetcodes

---
+++ src/main/java/Q00653s.java
@@ -0,0 +1,56 @@
+import tool.TreeNode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 给定一个二叉搜索树和一个目标结果，如果 BST 中存在两个元素且它们的和等于给定的目标结果，则返回 true。
+ *
+ * 案例 1:
+ *
+ * 输入:
+ *     5
+ *    / \
+ *   3   6
+ *  / \   \
+ * 2   4   7
+ *
+ * Target = 9
+ *
+ * 输出: True
+ *
+ * 案例 2:
+ *
+ * 输入:
+ *     5
+ *    / \
+ *   3   6
+ *  / \   \
+ * 2   4   7
+ *
+ * Target = 28
+ *
+ * 输出: False
+ *
+ * Constraints:
+ * The number of nodes in the tree is in the range [1, 104].
+ * -104 <= Node.val <= 104
+ * root is guaranteed to be a valid binary search tree.
+ * -105 <= k <= 105
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/two-sum-iv-input-is-a-bst
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Q00653s {
+
+    private Set<Integer> set = new HashSet<>();
+
+    public boolean findTarget(TreeNode root, int k) {
+        if (root == null) return false;
+        boolean res = findTarget(root.left, k);
+        if (res || set.contains(k - root.val)) return true;
+        set.add(root.val);
+        return findTarget(root.right, k);
+    }
+}
