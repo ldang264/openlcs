a417a89c54071d450453f6d03fa269634fcf20ec
ldang264 <820704815@qq.com>
2021-08-02 18:24:56 +0800

    leetcodes

--- src/main/java/Q00400m.java
+++ src/main/java/Q00400m.java
@@ -20,12 +20,26 @@
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 public class Q00400m {
-        // 1 1 = 1
-        // 2 1 + 1 * 9 = 1 + 9 * 1 * 10 ^ 0
-        // 3 9 + 2 * 90 = 10 + 9 * 2 * 10^ 1
-        // 4 189 + 3 * 900 = 190 + 9 * 3 * 10^2
-        // n = 56  prev = 9  len = 2 ten = 10
+    // 1 1 = 1
+    // 2 1 + 1 * 9 = 1 + 9 * 1 * 10 ^ 0
+    // 3 9 + 2 * 90 = 10 + 9 * 2 * 10^ 1
+    // 4 189 + 3 * 900 = 190 + 9 * 3 * 10^2
+    // n = 56  prev = 9  len = 2 ten = 10
     public int findNthDigit(int n) {
+        int l = 0, p, len = 1, ten = 1;
+        while ((p = 9 * len * ten) > l && p < n) { // 一直减掉差比数列的当前项
+            n -= p;
+            l = p;
+            ten *= 10;
+            len++;
+        }
+        n--;
+        int num = n / len + ten; // 第n位对应的整数
+        int offset = n % len; // 在整数中的偏移位置
+        return Integer.toString(num).charAt(offset) - '0';
+    }
+
+    public int findNthDigit1(int n) {
         int prev = 1, next = 1, len = 1, ten = 1;
         while (true) {
             prev = next; // 计算其实值
--- src/test/java/Q00400mTest.java
+++ src/test/java/Q00400mTest.java
@@ -9,11 +9,11 @@ public class Q00400mTest {
 
     @Test
     public void findNthDigit() {
+        Assert.assertEquals(q00400m.findNthDigit(Integer.MAX_VALUE), 2);
         Assert.assertEquals(q00400m.findNthDigit(10), 1);
         Assert.assertEquals(q00400m.findNthDigit(1), 1);
         Assert.assertEquals(q00400m.findNthDigit(9), 9);
         Assert.assertEquals(q00400m.findNthDigit(11), 0);
         Assert.assertEquals(q00400m.findNthDigit(13), 1);
-        Assert.assertEquals(q00400m.findNthDigit(Integer.MAX_VALUE), 2);
     }
 }
