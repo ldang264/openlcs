7f4f02efebdb25aa4b5665cf8dbac28cf9ed1b61
党羚(ldang) <820704815@qq.com>
2022-11-03 18:14:00 +0800

    J-62 question ans

---
+++ src/main/java/ans/J00062s_Ans.java
@@ -0,0 +1,9 @@
+class J00062s_Ans {
+    public int lastRemaining(int n, int m) {
+        int f = 0;
+        for (int i = 2; i != n + 1; ++i) {
+            f = (m + f) % i;
+        }
+        return f;
+    }
+}
