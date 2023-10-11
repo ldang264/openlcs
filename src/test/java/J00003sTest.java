a033a595a06034ce7c49dd4109d2f435b4586545
党羚(ldang) <820704815@qq.com>
2023-10-11 20:39:19 +0800

    opt

--- src/test/java/J00003sTest.java
+++ src/test/java/J00003sTest.java
@@ -2,12 +2,11 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class J00003sTest {
-    J00003s fpn = new J00003s();
 
     @Test
     public void findRepeatNumber() {
         int[] nums = new int[]{2, 3, 1, 0, 2, 5, 3};
-        int i = fpn.findRepeatNumber(nums);
+        int i = new J00003s().findRepeatNumber(nums);
         Assert.assertTrue(i == 2 || i == 3);
     }
 }
