c7572d5e6d762eb8a324ee66a7e8e015d124f120
党羚(ldang) <820704815@qq.com>
2023-10-20 08:38:25 +0800

    Update J00003sTest.java

--- src/test/java/J00003sTest.java
+++ src/test/java/J00003sTest.java
@@ -3,6 +3,9 @@ import org.junit.Test;
 
 public class J00003sTest {
 
+    /**
+    * 查找重复数字
+    */
     @Test
     public void findRepeatNumber() {
         int[] nums = new int[]{2, 3, 1, 0, 2, 5, 3};
