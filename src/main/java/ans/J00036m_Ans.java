37e4025dbb158a41d0f06335ac5ed2bf71206db8
党羚(ldang) <820704815@qq.com>
2023-10-31 08:18:01 +0800

    optimize

--- src/main/java/ans/J00036m_Ans.java
+++ src/main/java/ans/J00036m_Ans.java
@@ -7,9 +7,7 @@ package ans;
  * 下图展示了上面的二叉搜索树转化成的链表。“head” 表示指向链表中有最小元素的节点。
  *
  * 特别地，我们希望可以就地完成转换操作。当转化完成以后，树中节点的左指针需要指向前驱，树中节点的右指针需要指向后继。还需要返回链表中的第一个节点的指针。
- *
  * 注意：本题与主站 426 题相同：https://leetcode-cn.com/problems/convert-binary-search-tree-to-sorted-doubly-linked-list/
- *
  * 注意：此题对比原题有改动。
  *
  * 来源：力扣（LeetCode）
@@ -27,12 +25,12 @@ public class J00036m_Ans {
         }
 
         dfs(root);
-    /*
+        /*
         后续处理：
             让 head的左子指针 指向 最后一个节点
             让 最后一个节点的右子指针 指向 head
         形成闭环
-    */
+        */
         head.left = preNode;
         preNode.right = head;
         return head;
