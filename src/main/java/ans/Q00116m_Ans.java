45e84e3153c1937efbed243709a403a0f06d528a
党羚(ldang) <820704815@qq.com>
2023-11-01 08:19:56 +0800

    Q00116m_Ans

--- src/main/java/ans/Q00116m_Ans.java
+++ src/main/java/ans/Q00116m_Ans.java
@@ -16,11 +16,7 @@ import java.util.Queue;
  *
  * 初始状态下，所有 next 指针都被设置为 NULL。
  *
- *  
- *
  * 示例：
- *
- *
  *
  * 输入：{"$id":"1","left":{"$id":"2","left":{"$id":"3","left":null,"next":null,"right":null,"val":4},"next":null,"right":{"$id":"4","left":null,"next":null,"right":null,"val":5},"val":2},"next":null,"right":{"$id":"5","left":{"$id":"6","left":null,"next":null,"right":null,"val":6},"next":null,"right":{"$id":"7","left":null,"next":null,"right":null,"val":7},"val":3},"val":1}
  *
@@ -28,7 +24,6 @@ import java.util.Queue;
  *
  * 解释：给定二叉树如图 A 所示，你的函数应该填充它的每个 next 指针，以指向其下一个右侧节点，如图 B 所示。
  *  
- *
  * 提示：
  *
  * 你只能使用常量级额外空间。
