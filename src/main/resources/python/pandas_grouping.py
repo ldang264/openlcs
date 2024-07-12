e673c4630f471c8c865385016081b40cdbbab3c8
党羚(ldang) <820704815@qq.com>
2024-07-12 08:32:37 +0800

    grouping

---
+++ src/main/resources/python/pandas_grouping.py
@@ -0,0 +1,41 @@
+In [87]: df = pd.DataFrame(
+   ....:     {
+   ....:         "A": ["foo", "bar", "foo", "bar", "foo", "bar", "foo", "foo"],
+   ....:         "B": ["one", "one", "two", "three", "two", "two", "one", "three"],
+   ....:         "C": np.random.randn(8),
+   ....:         "D": np.random.randn(8),
+   ....:     }
+   ....: )
+   ....: 
+
+
+In [88]: df
+Out[88]: 
+     A      B         C         D
+0  foo    one  1.346061 -1.577585
+1  bar    one  1.511763  0.396823
+2  foo    two  1.627081 -0.105381
+3  bar  three -0.990582 -0.532532
+4  foo    two -0.441652  1.453749
+5  bar    two  1.211526  1.208843
+6  foo    one  0.268520 -0.080952
+7  foo  three  0.024580 -0.264610
+
+In [89]: df.groupby("A")[["C", "D"]].sum()
+Out[89]: 
+            C         D
+A                      
+bar  1.732707  1.073134
+foo  2.824590 -0.574779
+
+
+In [90]: df.groupby(["A", "B"]).sum()
+Out[90]: 
+                  C         D
+A   B                        
+bar one    1.511763  0.396823
+    three -0.990582 -0.532532
+    two    1.211526  1.208843
+foo one    1.614581 -1.658537
+    three  0.024580 -0.264610
+    two    1.185429  1.348368
