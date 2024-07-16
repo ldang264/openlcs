00210b972033259739ee9df7659f658d60da15cf
党羚(ldang) <820704815@qq.com>
2024-07-16 08:22:04 +0800

    reshaping

---
+++ src/main/resources/python/reshaping.py
@@ -0,0 +1,62 @@
+In [91]: arrays = [
+   ....:    ["bar", "bar", "baz", "baz", "foo", "foo", "qux", "qux"],
+   ....:    ["one", "two", "one", "two", "one", "two", "one", "two"],
+   ....: ]
+   ....: 
+
+In [92]: index = pd.MultiIndex.from_arrays(arrays, names=["first", "second"])
+
+In [93]: df = pd.DataFrame(np.random.randn(8, 2), index=index, columns=["A", "B"])
+
+In [94]: df2 = df[:4]
+
+In [95]: df2
+Out[95]: 
+                     A         B
+first second                    
+bar   one    -0.727965 -0.589346
+      two     0.339969 -0.693205
+baz   one    -0.339355  0.593616
+      two     0.884345  1.591431
+
+In [96]: stacked = df2.stack(future_stack=True)
+
+In [97]: stacked
+Out[97]: 
+first  second   
+bar    one     A   -0.727965
+               B   -0.589346
+       two     A    0.339969
+               B   -0.693205
+baz    one     A   -0.339355
+               B    0.593616
+       two     A    0.884345
+               B    1.591431
+dtype: float64
+
+In [98]: stacked.unstack()
+Out[98]: 
+                     A         B
+first second                    
+bar   one    -0.727965 -0.589346
+      two     0.339969 -0.693205
+baz   one    -0.339355  0.593616
+      two     0.884345  1.591431
+
+In [99]: stacked.unstack(1)
+Out[99]: 
+second        one       two
+first                      
+bar   A -0.727965  0.339969
+      B -0.589346 -0.693205
+baz   A -0.339355  0.884345
+      B  0.593616  1.591431
+
+In [100]: stacked.unstack(0)
+Out[100]: 
+first          bar       baz
+second                      
+one    A -0.727965 -0.339355
+       B -0.589346  0.593616
+two    A  0.339969  0.884345
+       B -0.693205  1.591431
