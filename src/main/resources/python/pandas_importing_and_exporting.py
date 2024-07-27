6c526931f7778714ee2215520eff74f35338492f
党羚(ldang) <820704815@qq.com>
2024-07-28 07:33:28 +0800

    excel

--- src/main/resources/python/pandas_importing_and_exporting.py
+++ src/main/resources/python/pandas_importing_and_exporting.py
@@ -32,3 +32,18 @@ Out[138]:
 8  4  4  1  0  1
 9  0  4  3  0  3
 
+In [139]: df.to_excel("foo.xlsx", sheet_name="Sheet1")
+
+In [140]: pd.read_excel("foo.xlsx", "Sheet1", index_col=None, na_values=["NA"])
+Out[140]: 
+   Unnamed: 0  0  1  2  3  4
+0           0  4  3  1  1  2
+1           1  1  0  2  3  2
+2           2  1  4  2  1  2
+3           3  0  4  0  2  2
+4           4  4  2  2  3  4
+5           5  4  0  4  3  1
+6           6  2  1  2  0  3
+7           7  4  0  4  4  4
+8           8  4  4  1  0  1
+9           9  0  4  3  0  3
