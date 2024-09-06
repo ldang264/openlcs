01852c6ccd5fd664eac7e8f97361e47ece491e19
党羚(ldang) <820704815@qq.com>
2024-09-06 09:10:15 +0800

    set copy on write

--- src/main/resources/python/pandas_copy_on_write.py
+++ src/main/resources/python/pandas_copy_on_write.py
@@ -10,3 +10,18 @@ Out[4]:
 0  100    4
 1    2    5
 2    3    6
+
+In [5]: pd.options.mode.copy_on_write = True
+
+In [6]: df = pd.DataFrame({"foo": [1, 2, 3], "bar": [4, 5, 6]})
+
+In [7]: subset = df["foo"]
+
+In [8]: subset.iloc[0] = 100
+
+In [9]: df
+Out[9]: 
+   foo  bar
+0    1    4
+1    2    5
+2    3    6
