2b5b33c2db71474631dc8315d478e5d17ad9cf59
党羚(ldang) <820704815@qq.com>
2024-09-11 09:10:32 +0800

    merge

---
+++ src/main/resources/python/pandas_merge.py
@@ -0,0 +1,74 @@
+In [1]: df1 = pd.DataFrame(
+   ...:     {
+   ...:         "A": ["A0", "A1", "A2", "A3"],
+   ...:         "B": ["B0", "B1", "B2", "B3"],
+   ...:         "C": ["C0", "C1", "C2", "C3"],
+   ...:         "D": ["D0", "D1", "D2", "D3"],
+   ...:     },
+   ...:     index=[0, 1, 2, 3],
+   ...: )
+   ...: 
+
+In [2]: df2 = pd.DataFrame(
+   ...:     {
+   ...:         "A": ["A4", "A5", "A6", "A7"],
+   ...:         "B": ["B4", "B5", "B6", "B7"],
+   ...:         "C": ["C4", "C5", "C6", "C7"],
+   ...:         "D": ["D4", "D5", "D6", "D7"],
+   ...:     },
+   ...:     index=[4, 5, 6, 7],
+   ...: )
+   ...: 
+
+In [3]: df3 = pd.DataFrame(
+   ...:     {
+   ...:         "A": ["A8", "A9", "A10", "A11"],
+   ...:         "B": ["B8", "B9", "B10", "B11"],
+   ...:         "C": ["C8", "C9", "C10", "C11"],
+   ...:         "D": ["D8", "D9", "D10", "D11"],
+   ...:     },
+   ...:     index=[8, 9, 10, 11],
+   ...: )
+   ...: 
+
+In [4]: frames = [df1, df2, df3]
+
+In [5]: result = pd.concat(frames)
+
+In [6]: result
+Out[6]: 
+      A    B    C    D
+0    A0   B0   C0   D0
+1    A1   B1   C1   D1
+2    A2   B2   C2   D2
+3    A3   B3   C3   D3
+4    A4   B4   C4   D4
+5    A5   B5   C5   D5
+6    A6   B6   C6   D6
+7    A7   B7   C7   D7
+8    A8   B8   C8   D8
+9    A9   B9   C9   D9
+10  A10  B10  C10  D10
+11  A11  B11  C11  D11
+
+In [7]: df4 = pd.DataFrame(
+   ...:     {
+   ...:         "B": ["B2", "B3", "B6", "B7"],
+   ...:         "D": ["D2", "D3", "D6", "D7"],
+   ...:         "F": ["F2", "F3", "F6", "F7"],
+   ...:     },
+   ...:     index=[2, 3, 6, 7],
+   ...: )
+   ...: 
+
+In [8]: result = pd.concat([df1, df4], axis=1)
+
+In [9]: result
+Out[9]: 
+     A    B    C    D    B    D    F
+0   A0   B0   C0   D0  NaN  NaN  NaN
+1   A1   B1   C1   D1  NaN  NaN  NaN
+2   A2   B2   C2   D2   B2   D2   F2
+3   A3   B3   C3   D3   B3   D3   F3
+6  NaN  NaN  NaN  NaN   B6   D6   F6
+7  NaN  NaN  NaN  NaN   B7   D7   F7
