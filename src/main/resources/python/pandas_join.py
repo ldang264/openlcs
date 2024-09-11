0bb236ab75e92cfe1c3dbd1c29bc0ce311923aa5
党羚(ldang) <820704815@qq.com>
2024-09-11 09:11:41 +0800

    Create pandas_join.py

---
+++ src/main/resources/python/pandas_join.py
@@ -0,0 +1,57 @@
+In [83]: left = pd.DataFrame(
+   ....:     {"A": ["A0", "A1", "A2"], "B": ["B0", "B1", "B2"]}, index=["K0", "K1", "K2"]
+   ....: )
+   ....: 
+
+In [84]: right = pd.DataFrame(
+   ....:     {"C": ["C0", "C2", "C3"], "D": ["D0", "D2", "D3"]}, index=["K0", "K2", "K3"]
+   ....: )
+   ....: 
+
+In [85]: result = left.join(right)
+
+In [86]: result
+Out[86]: 
+     A   B    C    D
+K0  A0  B0   C0   D0
+K1  A1  B1  NaN  NaN
+K2  A2  B2   C2   D2
+
+In [87]: result = left.join(right, how="outer")
+
+In [88]: result
+Out[88]: 
+      A    B    C    D
+K0   A0   B0   C0   D0
+K1   A1   B1  NaN  NaN
+K2   A2   B2   C2   D2
+K3  NaN  NaN   C3   D3
+
+In [89]: result = left.join(right, how="inner")
+
+In [90]: result
+Out[90]: 
+     A   B   C   D
+K0  A0  B0  C0  D0
+K2  A2  B2  C2  D2
+
+In [91]: left = pd.DataFrame(
+   ....:     {
+   ....:         "A": ["A0", "A1", "A2", "A3"],
+   ....:         "B": ["B0", "B1", "B2", "B3"],
+   ....:         "key": ["K0", "K1", "K0", "K1"],
+   ....:     }
+   ....: )
+   ....: 
+
+In [92]: right = pd.DataFrame({"C": ["C0", "C1"], "D": ["D0", "D1"]}, index=["K0", "K1"])
+
+In [93]: result = left.join(right, on="key")
+
+In [94]: result
+Out[94]: 
+    A   B key   C   D
+0  A0  B0  K0  C0  D0
+1  A1  B1  K1  C1  D1
+2  A2  B2  K0  C0  D0
+3  A3  B3  K1  C1  D1
