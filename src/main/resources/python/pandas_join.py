8f0dbd39769c2722d20c19d68648994d3dba0ae7
党羚(ldang) <820704815@qq.com>
2024-10-11 09:08:13 +0800

    merge and join

--- src/main/resources/python/pandas_join.py
+++ src/main/resources/python/pandas_join.py
@@ -1,3 +1,37 @@
+In [91]: left = pd.DataFrame(
+   ....:     {
+   ....:         "A": ["A0", "A1", "A2", "A3"],
+   ....:         "B": ["B0", "B1", "B2", "B3"],
+   ....:         "key": ["K0", "K1", "K0", "K1"],
+   ....:     }
+   ....: )
+   ....: 
+
+In [92]: right = pd.DataFrame({"C": ["C0", "C1"], "D": ["D0", "D1"]}, index=["K0", "K1"])
+
+In [93]: result = left.join(right, on="key")
+
+In [94]: result
+Out[94]: 
+    A   B key   C   D
+0  A0  B0  K0  C0  D0
+1  A1  B1  K1  C1  D1
+2  A2  B2  K0  C0  D0
+3  A3  B3  K1  C1  D1
+
+In [95]: result = pd.merge(
+   ....:     left, right, left_on="key", right_index=True, how="left", sort=False
+   ....: )
+   ....: 
+
+In [96]: result
+Out[96]: 
+    A   B key   C   D
+0  A0  B0  K0  C0  D0
+1  A1  B1  K1  C1  D1
+2  A2  B2  K0  C0  D0
+3  A3  B3  K1  C1  D1
+
 In [87]: result = left.join(right, how="outer")
 
 In [88]: result
