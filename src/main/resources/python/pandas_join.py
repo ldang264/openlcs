ac3dc7cce84655a578de7ce7db98477b9a0c0491
党羚(ldang) <820704815@qq.com>
2024-10-12 09:11:35 +0800

    multi join

--- src/main/resources/python/pandas_join.py
+++ src/main/resources/python/pandas_join.py
@@ -1,3 +1,33 @@
+In [97]: left = pd.DataFrame(
+   ....:     {
+   ....:         "A": ["A0", "A1", "A2", "A3"],
+   ....:         "B": ["B0", "B1", "B2", "B3"],
+   ....:         "key1": ["K0", "K0", "K1", "K2"],
+   ....:         "key2": ["K0", "K1", "K0", "K1"],
+   ....:     }
+   ....: )
+   ....: 
+
+In [98]: index = pd.MultiIndex.from_tuples(
+   ....:     [("K0", "K0"), ("K1", "K0"), ("K2", "K0"), ("K2", "K1")]
+   ....: )
+   ....: 
+
+In [99]: right = pd.DataFrame(
+   ....:     {"C": ["C0", "C1", "C2", "C3"], "D": ["D0", "D1", "D2", "D3"]}, index=index
+   ....: )
+   ....: 
+
+In [100]: result = left.join(right, on=["key1", "key2"])
+
+In [101]: result
+Out[101]: 
+    A   B key1 key2    C    D
+0  A0  B0   K0   K0   C0   D0
+1  A1  B1   K0   K1  NaN  NaN
+2  A2  B2   K1   K0   C1   D1
+3  A3  B3   K2   K1   C3   D3
+
 In [91]: left = pd.DataFrame(
    ....:     {
    ....:         "A": ["A0", "A1", "A2", "A3"],
