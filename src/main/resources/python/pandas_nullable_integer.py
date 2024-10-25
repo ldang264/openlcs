d48d106bdadf7089c2b2d2a5eea5ff5eecf59770
党羚(ldang) <820704815@qq.com>
2024-10-25 08:59:54 +0800

    oper

--- src/main/resources/python/pandas_nullable_integer.py
+++ src/main/resources/python/pandas_nullable_integer.py
@@ -1,4 +1,43 @@
-In [1]: arr = pd.array([1, 2, None], dtype=pd.Int64Dtype())
+In [12]: s = pd.Series([1, 2, None], dtype="Int64")
+
+# arithmetic
+In [13]: s + 1
+Out[13]: 
+0       2
+1       3
+2    <NA>
+dtype: Int64
+
+# comparison
+In [14]: s == 1
+Out[14]: 
+0     True
+1    False
+2     <NA>
+dtype: boolean
+
+# slicing operation
+In [15]: s.iloc[1:3]
+Out[15]: 
+1       2
+2    <NA>
+dtype: Int64
+
+# operate with other dtypes
+In [16]: s + s.iloc[1:3].astype("Int8")
+Out[16]: 
+0    <NA>
+1       4
+2    <NA>
+dtype: Int64
+
+# coerce when needed
+In [17]: s + 0.01
+Out[17]: 
+0    1.01
+1    2.01
+2    <NA>
+dtype: Float64In [1]: arr = pd.array([1, 2, None], dtype=pd.Int64Dtype())
 
 In [2]: arr
 Out[2]: 
