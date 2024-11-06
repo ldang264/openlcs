2ae60e13fb3fa182311ff818ddb18bbeb4b70816
党羚(ldang) <820704815@qq.com>
2024-11-06 08:29:21 +0800

    or

--- src/main/resources/python/pandas_nullable_boolean.py
+++ src/main/resources/python/pandas_nullable_boolean.py
@@ -12,3 +12,17 @@ Out[4]:
 0    1
 2    3
 dtype: int64
+
+In [5]: pd.Series([True, False, np.nan], dtype="object") | True
+Out[5]: 
+0     True
+1     True
+2    False
+dtype: bool
+
+In [6]: pd.Series([True, False, np.nan], dtype="boolean") | True
+Out[6]: 
+0    True
+1    True
+2    True
+dtype: boolean
