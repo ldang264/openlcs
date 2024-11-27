f7111f1c1410f14bf5f7b95e70bfa68f80d81022
党羚(ldang) <820704815@qq.com>
2024-11-27 22:31:16 +0800

    hist

--- src/main/resources/python/chart.py
+++ src/main/resources/python/chart.py
@@ -13,3 +13,25 @@ In [8]: df = df.cumsum()
 In [9]: plt.figure();
 
 In [10]: df.plot();
+
+In [25]: df4 = pd.DataFrame(
+   ....:     {
+   ....:         "a": np.random.randn(1000) + 1,
+   ....:         "b": np.random.randn(1000),
+   ....:         "c": np.random.randn(1000) - 1,
+   ....:     },
+   ....:     columns=["a", "b", "c"],
+   ....: )
+   ....: 
+
+In [26]: plt.figure();
+
+In [27]: df4.plot.hist(alpha=0.5);
+
+In [28]: plt.figure();
+
+In [29]: df4.plot.hist(stacked=True, bins=20);
+
+In [30]: plt.figure();
+
+In [31]: df4["a"].plot.hist(orientation="horizontal", cumulative=True);
