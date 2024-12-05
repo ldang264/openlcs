4c1df81f8af106d9f951912cc3b27f226d6d7b91
党羚(ldang) <820704815@qq.com>
2024-12-05 21:47:39 +0800

    rolling

--- src/main/resources/python/pandas_window.py
+++ src/main/resources/python/pandas_window.py
@@ -1,3 +1,29 @@
+In [8]: def weighted_mean(x):
+   ...:     arr = np.ones((1, x.shape[1]))
+   ...:     arr[:, :2] = (x[:, :2] * x[:, 2]).sum(axis=0) / x[:, 2].sum()
+   ...:     return arr
+   ...: 
+
+In [9]: df = pd.DataFrame([[1, 2, 0.6], [2, 3, 0.4], [3, 4, 0.2], [4, 5, 0.7]])
+
+In [10]: df.rolling(2, method="table", min_periods=0).apply(weighted_mean, raw=True, engine="numba")  # noqa: E501
+Out[10]: 
+          0         1    2
+0  1.000000  2.000000  1.0
+1  1.800000  2.000000  1.0
+2  3.333333  2.333333  1.0
+3  1.555556  7.000000  1.0
+
+In [11]: df = pd.DataFrame([[1, 2, 0.6], [2, 3, 0.4], [3, 4, 0.2], [4, 5, 0.7]])
+
+In [12]: df.ewm(0.5).mean()
+Out[12]: 
+          0         1         2
+0  1.000000  2.000000  0.600000
+1  1.750000  2.750000  0.450000
+2  2.615385  3.615385  0.276923
+3  3.550000  4.550000  0.562500
+
 In [1]: s = pd.Series(range(5))
 
 In [2]: s.rolling(window=2).sum()
